c39b5a6cd8ca647fe13631253a2295a495736ba3
Jonny McCoubrey <jonnymccoubrey@gmail.com>
2026-03-11 10:36:12 +0000

    Initial commit

---
+++ src/test/java/org/wod/gauge/wod_gauge_bridge/WodGuageBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wod.gauge.wod_gauge_bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WodGuageBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
